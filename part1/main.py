36cf28306200cda74eaec677709f660ef7d794d5
Dana Nikolaeva Chambourova <dnicolaeva@hotmail.com>
2016-11-11 15:56:09 -0600

    temp

--- part1/main.py
+++ part1/main.py
@@ -1,7 +1,16 @@
 import parser
 
 TEST_OPTION = 'test'
-TRANING_OPTION = 'traning'
+TRAINING_OPTION = 'training'
 
 if __name__ == "__main__":
-	imgs, labels = parser.parse_data(TRANING_OPTION)
+    ## TRAINING DATA
+    TR_imgs, TR_labels = parser.parse_data(TRAINING_OPTION)
+    TR_groups = parser.group_imgs(TR_imgs, TR_labels)
+    TR_priors = parser.get_priors(TR_groups)
+    TR_likelyhoods = parser.get_likelyhoods(TR_groups)
+
+    # TESTING DATA
+    TS_imgs, TS_labels = parser.parse_data(TEST_OPTION)
+    TS_output_label = parser.classify(TR_likelyhoods, TS_imgs)
+    stats = parser.analyse(TS_labels, TS_output_label)
